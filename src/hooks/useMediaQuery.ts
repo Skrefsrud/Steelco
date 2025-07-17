680944414c01c2e8ae9522c599dd4c107bf0885e
g.skrefsrud <g.skrefsrud@gmail.com>
2025-07-17 20:34:12 +0200

    lesmer og mindre

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,24 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export function useMediaQuery(query: string): boolean {
+    const [matches, setMatches] = useState<boolean>(false);
+
+    useEffect(() => {
+        const media = window.matchMedia(query);
+        if (media.matches !== matches) {
+            setMatches(media.matches);
+        }
+        const listener = () => {
+            setMatches(media.matches);
+        };
+        // Add event listener
+        media.addEventListener('change', listener);
+        // Remove event listener on cleanup
+
+        return () => media.removeEventListener('change', listener);
+    }, [matches, query]);
+
+    return matches;
+}
